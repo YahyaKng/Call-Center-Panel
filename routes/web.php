a3b647a5523c29ec8b89b53f2ad697caa3f9a01a
YahyaKng <yhy.kng@gmail.com>
2019-05-18 18:03:18 +0430

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Auth::routes();
 
@@ -28,6 +28,11 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::resource('team', 'TeamController', ['except' => ['show']]);
 	Route::get('team-edit', ['as' => 'team.edit', 'uses' => 'TeamController@edit']);
 	Route::put('team-edit', ['as' => 'team.update', 'uses' => 'TeamController@update']);
+
+	Route::resource('rest', 'RestController', ['except' => ['show']]);
+	
+	// Request a REST break
+	Route::get('/check-rest/{type}', 'RestController@rest');
 });
 
 // Route::group(['middleware' => 'auth'], function () {
