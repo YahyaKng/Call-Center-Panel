b33e47594379cd656110c976c2944fc11b025264
YahyaKng <yhy.kng@gmail.com>
2019-05-19 10:36:38 +0430

    RestHistory model added

---
+++ app/RestHistory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RestHistory extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'team_id',
+        'rest_type',
+        'rest_status',
+    ];
+
+    /**
+     * Get the user that wants to have break.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the team of user.
+     */
+    public function team()
+    {
+        return $this->belongsTo('App\Team');
+    }
+}
