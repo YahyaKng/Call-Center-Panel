c95d601b962582df4ecaf7d6a553b615b2f11691
YahyaKng <yhy.kng@gmail.com>
2019-06-24 17:10:11 +0430

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // my admin middleware
+        'admin' => 'App\Http\Middleware\AdminMiddleware',
     ];
 
     /**
